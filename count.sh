ef43659812c68ef17bd58c9416d80c7144a35e2a
Ruifeng_li <49118497+Ruifengpku@users.noreply.github.com>
2024-02-02 10:32:40 +0800

    Delete count.sh

--- count.sh
+++
@@ -1,7 +0,0 @@
-for i in `ls ../mapping/MEF-P[147].bam`
-do
-{
-	nm=`basename $i`
-	htseq-count -f bam -r pos -s reverse -i gene_name $i /lustre/user/liclab/zhangc/Taolab/guan/rna-seq/reference/fasta/gencode.vM12.annotation.gtf > ${nm/.bam/.counts}
-}&
-done
