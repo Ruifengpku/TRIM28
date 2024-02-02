740e8f1decb3b748847e1dcc39619079217ef895
Ruifeng_li <49118497+Ruifengpku@users.noreply.github.com>
2024-02-02 10:33:53 +0800

    Delete homer.sh

--- homer.sh
+++
@@ -1,7 +0,0 @@
-#findMotifsGenome.pl KOUP_homer.tmp mm10 motif_KOUP/ -size -300,100  -len 8,10,12
-for i in `ls IRF8_p0.05_peaks.narrowPeak`
-do
-{
-	findMotifsGenome.pl $i mm10 ${i/.narrowPeak/}"distal_motif_unmark" -p 60 -preparse -size -300,100  -len 8,10,12
-}&
-done
